63cfd32937ce98a9ba9304dba7432f8b6d3f264b
JaeHyeon <ljh040513@naver.com>
2021-04-21 20:39:09 +0900

    [ADD] water detail page code add
    ADD - 수분 상세 화면 코드 추가

--- SmartFarm/app/src/main/java/com/example/smartfarm/MainActivity.kt
+++ SmartFarm/app/src/main/java/com/example/smartfarm/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.smartfarm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.smartfarm.databinding.ActivityMainBinding
+import com.example.smartfarm.databinding.ActivityWaterDetailBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.activity = this
     }
+
+    fun waterStateViewClick(){
+        val intent = Intent(this, WaterDetail::class.java)
+        intent.putExtra("waterValue", 50)
+        startActivity(intent)
+    }
+
 }
